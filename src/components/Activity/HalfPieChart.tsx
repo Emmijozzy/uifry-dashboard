aa9a7b39e4a2202f4ab44f95b07ec8c01323601c
Emmijozzy <josephogunsuyi360@gmail.com>
2023-05-15 14:55:49 -0700

    activity opyimize

--- src/components/Activity/HalfPieChart.tsx
+++ src/components/Activity/HalfPieChart.tsx
@@ -12,7 +12,7 @@ import pieData from '../../constant/pieData'
 const HalfPieChart = ({ /* see data tab */ }: any) => {
 
   return(
-    <div className='w-full h-full flex flex-col justify-around text-darkbg'>
+    <div className='flex flex-col justify-around w-full h-full text-darkbg'>
         <div className='w-full h-[230px] relative'>
           <ResponsivePie
             data={pieData}
@@ -30,18 +30,18 @@ const HalfPieChart = ({ /* see data tab */ }: any) => {
             arcLabelsSkipAngle={0}
             legends={[ ]}
           />
-          <div className='absolute left-[45%] bottom-[15%] text-2xl'>75%</div>
+          <div className='absolute left-[45%] bottom-[15%] text-2xl dark:text-white'>75%</div>
         </div>
       <div className='h-[40px] flex items-center justify-between px-4 md:mt-0 lg:mt-[-20px] '>
         <div className='w-[50%]'>
-          <div className='h-full flex space-x-2 items-center'>
+          <div className='flex items-center h-full space-x-2'>
             <div className='bg-primary h-[10px] w-[10px] rounded-full'></div>
             <p className='text-xs text-lighttext2 dark:text-darktext2'>Daily Payment</p>
           </div>
           <p className='text-sm'>55%</p>
         </div>
         <div>
-          <div className='h-full flex space-x-2 items-center'>
+          <div className='flex items-center h-full space-x-2'>
             <div className='bg-secondary h-[10px] w-[10px] rounded-full'></div>
             <p className='text-xs text-lighttext2 dark:text-darktext2'>Hobby</p>
           </div>
